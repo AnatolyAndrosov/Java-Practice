bf7cc2112ba4d3dcfd9bd812421258ead92ed2aa
Anatoly Androsov <coxman@mail.ru>
2023-11-21 22:49:57 +0300

    Add mosmetro.json

--- Data Collector 17.13/src/main/java/HtmlParser/Line.java
+++ Data Collector 17.13/src/main/java/HtmlParser/Line.java
@@ -1,5 +1,6 @@
 package HtmlParser;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@JsonPropertyOrder({"number", "mame"})
 public class Line {
 
     public static List<Line> lineList = new ArrayList<>();
--- Data Collector 17.13/src/main/java/Main.java
+++ Data Collector 17.13/src/main/java/Main.java
@@ -7,6 +7,7 @@ import JsonAndCSVParser.StationData;
 import JsonAndCSVParser.StationDepth;
 import org.jsoup.nodes.Document;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,14 +25,25 @@ public class Main {
         List<StationData> stationDataList = StationData.getStationDataList(foundedFiles);
         List<StationDepth> stationDepthList = StationDepth.getStationDepthList(foundedFiles);
         List<Station> stations = Station.getStationList(stationList, lines, stationDataList, stationDepthList);
-        Map<String, List<Station>> map = new HashMap<>();
-        map.put("stations", stations);
+        Map<String, Object> stationMap = new HashMap<>();
+        stationMap.put("stations", stations);
+        Map<String, Object> lineMap = new HashMap<>();
+        for (Line line : lines) {
+            List<String> stationsToLineList = new ArrayList<>();
+            for (Station station : stations) {
+                if (station.getLineNumber().equals(line.getNumber())) stationsToLineList.add(station.getName());
+            }
+            lineMap.put(line.getNumber(), stationsToLineList);
 
+        }
+
+        Map<String, Object> mskMetro = new HashMap<>();
+        mskMetro.put("stations", lineMap);
+        mskMetro.put("lines", lines);
 
         JSONWriter jsonWriter = new JSONWriter();
-        jsonWriter.writeJSONToFile(map, "stations.json");
-
+        jsonWriter.writeJSONToFile(stationMap, "stations.json");
+        jsonWriter.writeJSONToFile(mskMetro, "mosmetro.json");
 
     }
-
 }
