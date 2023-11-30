2f44cda09dcf32048ee17cdb23407b5f54afc5a2
Anatoly Androsov <coxman@mail.ru>
2023-11-30 20:58:03 +0300

    18.4 Done

---
+++ HibernateConnectionAndConfiguration18.4/src/main/java/Course.java
@@ -0,0 +1,27 @@
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Courses")
+@Setter
+@Getter
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int duration;
+    @Enumerated(EnumType.STRING)
+    private CoursesType type;
+    private String description;
+    @Column(name = "teacher_id")
+    private int teacherId;
+
+    @Column(name = "students_count")
+    private int studentsCount;
+    private int price;
+    @Column(name = "price_per_hour")
+    private float pricePerHour;
+
+}
