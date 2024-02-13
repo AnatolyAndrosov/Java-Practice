f09d354b7ce1dc9cf703e426e837dfe61194fb6a
Anatoly Androsov <coxman@mail.ru>
2024-02-14 00:10:29 +0300

    Add imageResizer

---
+++ ThreadClass 21.2/src/main/java/Main.java
@@ -0,0 +1,53 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Main {
+
+    private static final int newWidth = 300;
+
+    public static void main(String[] args) {
+        String srcFolder = "C:/Users/Anatoly/Desktop/src";
+        String dstFolder = "C:/Users/Anatoly/Desktop/dst";
+
+        File srcDir = new File(srcFolder);
+
+        long start = System.currentTimeMillis();
+
+        File[] files = srcDir.listFiles();
+
+        try {
+            for (File file : files) {
+                BufferedImage image = ImageIO.read(file);
+                if (image == null) {
+                    continue;
+                }
+                int newHeight = (int) Math.round(
+                        (double) image.getHeight() / (image.getWidth()
+                                / (double) newWidth));
+                BufferedImage newImage = new BufferedImage(newWidth,
+                        newHeight, BufferedImage.TYPE_INT_RGB);
+
+                int widthStep = image.getWidth() / newWidth;
+                int heightStep = image.getHeight() / newHeight;
+
+                for (int x = 0; x < newWidth; x++) {
+                    for (int y = 0; y < newHeight; y++) {
+                        int rgb = image.getRGB(x * widthStep,
+                                y * heightStep);
+                        newImage.setRGB(x, y, rgb);
+                    }
+                }
+
+                File newFile = new File(dstFolder + "/" + file.getName());
+                ImageIO.write(newImage, "jpg", newFile);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("Duration: " + (System.currentTimeMillis() - start));
+
+    }
+}
+
