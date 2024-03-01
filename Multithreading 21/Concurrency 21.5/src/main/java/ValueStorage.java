ae58c5714d25e658666822fc1b8f889a38217fd0
Anatoly Androsov <coxman@mail.ru>
2024-03-01 21:41:14 +0300

    21.5-6

--- Multithreading 21/Concurrency 21.5/src/main/java/ValueStorage.java
+++ Multithreading 21/Concurrency 21.5/src/main/java/ValueStorage.java
@@ -3,7 +3,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ValueStorage {
 //    private static int value;
 
-    private static AtomicInteger value = new AtomicInteger();
+    private static final AtomicInteger value = new AtomicInteger();
     public static void incrementValue() {
 //        value +=1;
         value.incrementAndGet();
