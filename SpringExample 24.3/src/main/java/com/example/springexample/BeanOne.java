a05d9524c60f8adf0917d0e077c5a7dbf36fa94a
Anatoly Androsov <coxman@mail.ru>
2024-08-12 21:59:28 +0300

    My first Spring Application

---
+++ SpringExample 24.3/src/main/java/com/example/springexample/BeanOne.java
@@ -0,0 +1,15 @@
+package com.example.springexample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanOne {
+
+    private BeanTwo beanTwo;
+    public BeanOne(BeanTwo beanTwo) {
+        this.beanTwo = beanTwo;
+    }
+    public void sayBeanOne() {
+        System.out.println("Я - бин номер один!");
+    }
+}
---
+++ SpringExample 24.3/src/main/java/com/example/springexample/BeanTwo.java
@@ -0,0 +1,10 @@
+package com.example.springexample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanTwo {
+    public void sayBeanTwo() {
+        System.out.println("Я - бин под номером два!");
+    }
+}
