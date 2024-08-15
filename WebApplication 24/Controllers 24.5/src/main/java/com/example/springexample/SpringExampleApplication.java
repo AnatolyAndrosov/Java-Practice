06b262b26173fc42da803a6bf4d8af56ff7d4d29
Anatoly Androsov <coxman@mail.ru>
2024-08-15 23:28:10 +0300

    add Controllers 24.5

---
+++ WebApplication 24/Controllers 24.5/src/main/java/com/example/springexample/SpringExampleApplication.java
@@ -0,0 +1,20 @@
+package com.example.springexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(SpringExampleApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringExampleApplication.class, args);
+        String[] beansNames = context.getBeanDefinitionNames();
+        System.out.println("Количество бинов " + beansNames.length);
+//        for (String name : beansNames) {
+//            System.out.println(name);
+//        }
+
+
+    }
+}
