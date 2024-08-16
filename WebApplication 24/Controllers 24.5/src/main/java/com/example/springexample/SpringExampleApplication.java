a1983d639422ef917b43a05f886b9a8db497df34
Anatoly Androsov <coxman@mail.ru>
2024-08-16 22:23:44 +0300

    create todolist

--- WebApplication 24/Controllers 24.5/src/main/java/com/example/springexample/SpringExampleApplication.java
+++ WebApplication 24/Controllers 24.5/src/main/java/com/example/springexample/SpringExampleApplication.java
@@ -8,6 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class SpringExampleApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringExampleApplication.class, args);
-        String[] beansNames = context.getBeanDefinitionNames();
     }
+
 }
--- WebApplication 24/Controllers 24.5/src/main/java/com/example/springexample/controllers/DefaultController.java
+++ WebApplication 24/Controllers 24.5/src/main/java/com/example/springexample/controllers/DefaultController.java
@@ -1,15 +1,71 @@
 package com.example.springexample.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.TreeMap;
+
+@RestController
 public class DefaultController {
 
-    @ResponseBody
-    @RequestMapping(path = "/hello")
-    public String helloSpring() {
-        return "Hello, Spring!";
+//    @ResponseBody
+//    @RequestMapping(path = "/hello")
+//    public String helloSpring() {
+//        return "Hello, Spring!";
+//    }
+
+//    @ResponseBody
+//    @RequestMapping(path = "/hello")
+//    public String helloSpring(@RequestParam String name) {
+//        return "Hello, Spring! Param value: " + name;
+//    }
+
+//    @ResponseBody
+//    @RequestMapping(path = "/hello/{name}")
+//    public String helloSpring(@PathVariable String name) {
+//        return "Hello, Spring! Param value: " + name;
+//    }
+
+//    @ResponseBody
+//    @RequestMapping(path = "/hello", method = RequestMethod.POST)
+//    public String helloSpring(@RequestParam String name) {
+//        return "Hello, Spring! Param value: " + name;
+//    }
+
+    private TreeMap<Integer, String> todo;
+
+    public DefaultController() {
+        todo = new TreeMap<>();
+        todo.put(1, "Купить молока");
+        todo.put(2, "Покормить кота");
+
+    }
+
+    @GetMapping(path = "/todo")
+    public TreeMap<Integer, String> get() {
+        return todo;
+    }
+
+    @PostMapping(path = "/todo")
+    public ResponseEntity post(@RequestParam String item) {
+        if (item == null || item.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        todo.put(todo.lastKey() + 1, item);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/todo/{id}")
+    public ResponseEntity put (@PathVariable int id, @RequestParam String item) {
+        todo.put(id, item);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/todo/{id}")
+    public ResponseEntity put (@PathVariable int id) {
+        todo.remove(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
