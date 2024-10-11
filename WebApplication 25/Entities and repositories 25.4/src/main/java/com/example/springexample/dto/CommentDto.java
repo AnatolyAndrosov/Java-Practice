5bcf5ba9539ba4f67fd0dda16811a7a5df86cc9b
Anatoly Androsov <coxman@mail.ru>
2024-10-11 19:53:09 +0300

    Done 25.4

---
+++ WebApplication 25/Entities and repositories 25.4/src/main/java/com/example/springexample/dto/CommentDto.java
@@ -0,0 +1,15 @@
+package com.example.springexample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentDto {
+    private Integer id;
+    private String text;
+    private String author;
+}
