b0586fb1cd267f4b104e390ad3add445203884ff
Anatoly Androsov <coxman@mail.ru>
2024-12-02 20:37:54 +0300

    Done Integration-tests fo SpringExampleApplicationTests 26.6

--- QA 26/TestSpringBootApplication 26.6/src/test/java/com/example/springexample/SpringExampleApplicationTests.java
+++ QA 26/TestSpringBootApplication 26.6/src/test/java/com/example/springexample/SpringExampleApplicationTests.java
@@ -1,18 +1,22 @@
 package com.example.springexample;
 
+import com.example.springexample.dto.CommentDto;
 import com.example.springexample.entity.Author;
 import com.example.springexample.entity.Comment;
 import com.example.springexample.repositories.AuthorRepository;
 import com.example.springexample.repositories.CommentRepository;
-import liquibase.util.StringClauses;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringExampleApplicationTests {
@@ -45,7 +49,7 @@ public class SpringExampleApplicationTests {
     }
 
     @BeforeEach
-    private void fillingDatabase() {
+    public void fillingDatabase() {
         Comment comment = new Comment();
         comment.setText("Комментарий");
         comment.setAuthor(authorRepository.save(new Author()));
@@ -55,5 +59,15 @@ public class SpringExampleApplicationTests {
     @AfterEach
     public void clearDatabase() {
         authorRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Test get comment by id")
+    public void testGetCommentById() {
+        ResponseEntity<CommentDto> response = template.getRestTemplate()
+                .getForEntity("http://localhost:" + port + "/comment/1", CommentDto.class);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(1, response.getBody().getId());
+        assertEquals("Комментарий", response.getBody().getText());
     }
 }
